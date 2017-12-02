b2c55f9a48cdd77e511a06c57103119289a76510
fon-Luchs <lvo123ua@gmail.com>
2017-12-03 01:50:47 +0200

    todo item add to todo list

---
+++ JavaScript_ToDo/javascript/app.js
@@ -0,0 +1,4 @@
+const todoForm  = document.getElementById('todo-form');
+const todoInput = document.getElementById('add-input');
+const todoList  = document.getElementById('todo-list');
+const todoItems = document.querySelectorAll('.todo-item');
